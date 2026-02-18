ffd562a29d0c8961e7afe9e554c290a40c6a3ad2
jcarpenter-uam <jcarpenter@uaminc.com>
2026-02-18 11:17:30 -0600

    conditional url

--- src/shared/network.js
+++ src/shared/network.js
@@ -1,5 +1,12 @@
-// export const BASE_DOMAIN = "translator.my-uam.com";
-export const BASE_DOMAIN = "2495-158-120-147-235.ngrok-free.app";
+import packageJson from "../../package.json";
 
-export const API_BASE_URL = `https://${BASE_DOMAIN}`;
-export const WS_BASE_URL = `wss://${BASE_DOMAIN}`;
+export const PROD_BASE_URL = "https://translator.my-uam.com";
+export const STAG_BASE_URL = "https://2495-158-120-147-235.ngrok-free.app";
+
+const APP_VERSION = packageJson.version ?? "";
+const IS_PRERELEASE = /-(alpha|beta|rc|pre)\.?/i.test(APP_VERSION);
+
+export const BASE_URL = IS_PRERELEASE ? STAG_BASE_URL : PROD_BASE_URL;
+export const BASE_DOMAIN = new URL(BASE_URL).host;
+export const API_BASE_URL = BASE_URL;
+export const WS_BASE_URL = BASE_URL.replace(/^http/i, "ws");
